c526bab19ec1ef969301151a22e7836f03d60247
Jisse Reitsma <jisse@yireo.com>
2024-10-07 10:09:21 +0200

    Add registration file

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::LIBRARY, 'Yireo_MagewireTestUtilsg', __DIR__);
